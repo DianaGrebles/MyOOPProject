3ececa41f8f8e7421cbc10c5dd44f7e02c2378df
DianaGrebles <diagrebles@gmail.com>
2023-12-20 13:30:59 +0200

    .

--- src/CarteTest.java
+++ src/CarteTest.java
@@ -19,11 +19,15 @@ public class CarteTest {
         return "Cartea" + titlu + " are ca autor";
     }
 
-    Carte carte1=new Carte();
+    Carte carte1 = new Carte();
     carte1.autor="Mihai Eminescu";
     carte1.titlu="Poezii";
-
-
-
+    Carte carte2 = new Carte();
+    carte2.titlu="Amintiri din copiarie";
+    carte2.autor="Ion Creanga";
 
 }
+
+
+
+
